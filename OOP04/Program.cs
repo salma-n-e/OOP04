30fca032343bfe871e731356476d071056090483
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-18 19:18:59 +0300

    Q 2

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace OOP04
+﻿using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+
+namespace OOP04
 {
     internal class Program
     {
@@ -9,7 +12,20 @@
             // only the essential features of an object.
             // b) It reduces code complexity, minimizes dependency tight-coupling, and improves maintainability
 
+            // Q 2
+            // a) Abstract Class: Serves as a base identity (is a relationship).
+            // It can contain state (fields), implementation details (concrete methods), constructors,
+            // access modifiers, and abstract method signatures.
 
+            // Interface: Defines a contract / behavior(can do capability).
+            // Historically contains only method and property declarations without instance state,
+            // though modern languages allow default implementations.
+
+            // b) Unrelated classes need to share a common behavior
+            // c) Multiple Abstract Classes: No. Most object oriented languages
+            // do not support multiple inheritance of classes .
+
+           // Multiple Interfaces: Yes.A single class can implement multiple interfaces simultaneously.
         }
     }
 }
