baeac7ee580bf47ee7cd38f2e89579e3638d423a
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-18 19:15:46 +0300

    Q 1

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-           
+            // Q 1
+            // a) focuses on hiding complex implementation details from the user and displaying
+            // only the essential features of an object.
+            // b) It reduces code complexity, minimizes dependency tight-coupling, and improves maintainability
+
+
         }
     }
 }
